c631aed3c6cd971fc5ae9bc3a14d09015b8ef581
Samuel Just <sam.just@inktank.com>
2014-01-22 17:57:34 -0800

    ReplicatedBackend: excise OSDService*
    This should eventually make it easier to mock out a PGBackend::Listener.
    
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/PGBackend.h
+++ src/osd/PGBackend.h
@@ -147,6 +147,26 @@
      virtual void update_stats(
        const pg_stat_t &stat) = 0;
 
+     virtual void schedule_work(
+       GenContext<ThreadPool::TPHandle&> *c) = 0;
+
+     virtual int whoami() const = 0;
+
+     virtual void send_message_osd_cluster(
+       int peer, Message *m, epoch_t from_epoch) = 0;
+     virtual void send_message_osd_cluster(
+       Message *m, Connection *con) = 0;
+     virtual void send_message_osd_cluster(
+       Message *m, const ConnectionRef& con) = 0;
+     virtual ConnectionRef get_con_osd_cluster(int peer, epoch_t from_epoch) = 0;
+     virtual entity_name_t get_cluster_msgr_name() = 0;
+
+     virtual PerfCounters *get_logger() = 0;
+
+     virtual tid_t get_tid() = 0;
+
+     virtual LogClientTemp clog_error() = 0;
+
      virtual ~Listener() {}
    };
    Listener *parent;
@@ -477,5 +497,29 @@
 		pair<bufferlist*, Context*> > > &to_read,
      Context *on_complete) = 0;
  };
+
+struct PG_SendMessageOnConn: public Context {
+  PGBackend::Listener *pg;
+  Message *reply;
+  ConnectionRef conn;
+  PG_SendMessageOnConn(
+    PGBackend::Listener *pg,
+    Message *reply,
+    ConnectionRef conn) : pg(pg), reply(reply), conn(conn) {}
+  void finish(int) {
+    pg->send_message_osd_cluster(reply, conn.get());
+  }
+};
+
+struct PG_QueueAsync : public Context {
+  PGBackend::Listener *pg;
+  GenContext<ThreadPool::TPHandle&> *c;
+  PG_QueueAsync(
+    PGBackend::Listener *pg,
+    GenContext<ThreadPool::TPHandle&> *c) : pg(pg), c(c) {}
+  void finish(int) {
+    pg->schedule_work(c);
+  }
+};
 
 #endif
