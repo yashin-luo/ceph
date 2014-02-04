3018c4d9c5b1167b71141e650e66ef2086c3846c
Samuel Just <sam.just@inktank.com>
2014-02-03 17:10:56 -0800

    ReplicatedPG: find_object_context debugging: output oi
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -202,6 +202,7 @@ void ReplicatedPG::on_local_recover(
   ObjectStore::Transaction *t
   )
 {
+  dout(10) << __func__ << ": " << hoid << dendl;
   ObjectRecoveryInfo recovery_info(_recovery_info);
   if (recovery_info.soid.snap < CEPH_NOSNAP) {
     assert(recovery_info.oi.snaps.size());
@@ -6556,7 +6557,8 @@ ObjectContextRef ReplicatedPG::get_object_context(const hobject_t& soid,
   ObjectContextRef obc = object_contexts.lookup(soid);
   if (obc) {
     dout(10) << "get_object_context " << obc << " " << soid
-	     << " " << obc->rwstate << dendl;
+	     << " " << obc->rwstate
+	     << " oi:" << obc->obs.oi << dendl;
   } else {
     // check disk
     bufferlist bv;
@@ -6614,6 +6616,7 @@ ObjectContextRef ReplicatedPG::get_object_context(const hobject_t& soid,
 
     dout(10) << "get_object_context " << obc << " " << soid
 	     << " " << obc->rwstate
+	     << " oi:" << obc->obs.oi
 	     << " 0 -> 1 read " << obc->obs.oi << dendl;
   }
   return obc;
@@ -6668,7 +6671,10 @@ int ReplicatedPG::find_object_context(const hobject_t& oid,
 	*pmissing = head;
       return -ENOENT;
     }
-    dout(10) << "find_object_context " << oid << " @" << oid.snap << dendl;
+    dout(10) << "find_object_context " << oid
+	     << " @" << oid.snap
+	     << " oi=" << obc->obs.oi
+	     << dendl;
     *pobc = obc;
 
     // always populate ssc for SNAPDIR...
@@ -6686,7 +6692,10 @@ int ReplicatedPG::find_object_context(const hobject_t& oid,
 	*pmissing = head;
       return -ENOENT;
     }
-    dout(10) << "find_object_context " << oid << " @" << oid.snap << dendl;
+    dout(10) << "find_object_context " << oid
+	     << " @" << oid.snap
+	     << " oi=" << obc->obs.oi
+	     << dendl;
     *pobc = obc;
 
     if (can_create && !obc->ssc)
