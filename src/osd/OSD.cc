181eb3c5329ff7a154fcdd4835a332651f5fbf4c
Sage Weil <sage@redhat.com>
2014-08-22 11:52:26 -0700

    prevent random people from encoding the OSDMap
    This is a safety check: nobody should be encoding OSDMaps except:
    
     - the mons
     - MOSDMap, when encoding old OSDMaps with legacy encodings
     - ceph-dencoder
     - the OSD
    
    More importantly, nobody should be encoding OSDMap::Incremental except
    
     - the leader mon
    
    We use a reserved feature bit to indicate that the encoder is enlightened.
    Note that we skip this check for legacy encodings, so we don't touch
    MOSDMap in this patch.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -6237,7 +6237,7 @@ void OSD::handle_osd_map(MOSDMap *m)
 	     << dendl;
 
       bufferlist fbl;
-      o->encode(fbl);
+      o->encode(fbl, inc.encode_features | CEPH_FEATURE_RESERVED);
 
       hobject_t fulloid = get_osdmap_pobject_name(e);
       t.write(coll_t::META_COLL, fulloid, 0, fbl.length(), fbl);
